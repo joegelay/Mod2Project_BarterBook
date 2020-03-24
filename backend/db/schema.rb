c5fba005943c834991769e974a79f0be7f49537c
japerry911 <jack.e.perry@outlook.com>
2020-03-24 10:48:04 -0600

    html - added image_url column to users and seeds

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_153627) do
+ActiveRecord::Schema.define(version: 2020_03_24_164546) do
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_153627) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
   add_foreign_key "user_skills", "skills"
