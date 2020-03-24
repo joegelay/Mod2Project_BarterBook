5aa0140ce269864947cea408a5915ecddce8ce52
joegelay <joegelay@gmail.com>
2020-03-24 14:45:41 -0600

    add index to users controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def index 
+        @users = User.all 
+        render json: @users, inclide: :skills 
+    end 
+
+    def show
+        
+    end 
 end
