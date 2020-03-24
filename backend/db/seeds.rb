182a1d668947378bab559cfe8f75533837012bec
japerry911 <jack.e.perry@outlook.com>
2020-03-24 15:27:24 -0600

    index backend || index frontend page and js file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -10,7 +10,7 @@ UserSkill.destroy_all
 User.destroy_all
 Skill.destroy_all
 
-joe = User.create name: "Joe Gelay", email: "joegelay@gmail.com", password: "ahmed", zip: "80221", bio: "Looking to learn piano and teach Spanish!", image_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.mountainproject.com%2Fphotos%2Favatars%2F200584219.jpg%3F1567733024&imgrefurl=https%3A%2F%2Fwww.mountainproject.com%2Fuser%2F200584219%2Fjoe-gelay&tbnid=ac7c_-TQY6szSM&vet=12ahUKEwj3w7X5x7PoAhXTCc0KHXErDTwQMygAegUIARDSAQ..i&docid=Eh_gS60xbNYysM&w=200&h=133&itg=1&q=joe%20gelay&ved=2ahUKEwj3w7X5x7PoAhXTCc0KHXErDTwQMygAegUIARDSAQ"
+joe = User.create name: "Joe Gelay", email: "joegelay@gmail.com", password: "ahmed", zip: "80221", bio: "Looking to learn piano and teach Spanish!", image_url: "https://assets3.thrillist.com/v1/image/2547068/size/tmg-article_tall;jpeg_quality=20.jpg"
 jack = User.create name: "Jack Perry", email: "jack.perry@gmail.com", password: "ahmed", zip: "80206", bio: "Learning to code, will walk your dog!", image_url: "https://www.allthingsdogs.com/wp-content/uploads/2018/09/A-Dog-Smiling.jpg"
 ahmed = User.create name: "Ahemd Gaber", email: "ahmedgaber@gmail.com", password: "ahmed", zip: "90210", bio: "I can teach code, need someone to walk my dog!", image_url: "https://i.ytimg.com/vi/cYNlJYQI3Uw/maxresdefault.jpg"
 
