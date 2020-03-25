8c623219b23d79547c5ff157d21b8d61346dc6a7
japerry911 <jack.e.perry@outlook.com>
2020-03-25 08:26:22 -0600

    Cleaned up cards, and added search by name functionality in index search bar

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,8 @@
-fetch('http://localhost:4000/users')
+const searchParams = new URLSearchParams(window.location.search);
+const userSearch = searchParams.get('search');
+const userUrl = userSearch === null ? 'http://localhost:4000/users' : `http://localhost:4000/users?search=${userSearch}`;
+
+fetch(userUrl)
     .then(response => response.json())
     .then(users => showUsers(users));
 
@@ -7,6 +11,16 @@ showUsers = users => {
 
     users.forEach(user => {
         const userElement = document.createElement('div');
+        const skillsElement = document.createElement('ul');
+
+        user.skills.forEach(skill => {
+            const skillElement = document.createElement('li');
+
+            skillElement.className = 'skill-list-item';
+            skillElement.innerText = skill.name;
+            
+            skillsElement.appendChild(skillElement);
+        });
         
         userElement.className = 'user-card';
         userElement.innerHTML = `
@@ -19,10 +33,18 @@ showUsers = users => {
                 <div class="flip-card-back">
                     <h5 class="card-title">${user.name}</h5>
                     <p class="card-text">${user.bio}</p>
+                    <h6 class="card-text">Skills/Desired Skills:</h6>
+                    <div id='skills-list-${user.id}'></div>
+                    <hr>
+                    <a class="email-link" href="mailto:${user.email}" target="_blank" rel="noopener noreferrer"><i class="fa fa-envelope"></i> Email</a>
                 </div>
             </div>
         </div>`;
+
         usersDiv.append(userElement);
+
+        const userEleAdded = document.querySelector(`#skills-list-${user.id}`);
+        userEleAdded.append(skillsElement);
     });
 }
 
